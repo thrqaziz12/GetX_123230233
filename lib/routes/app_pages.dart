7af6a2a5c18cc5a87af70533a1f25a38b879ecb1
thrqaziz12 <161470094+thrqaziz12@users.noreply.github.com>
2026-05-17 14:47:09 +0700

    feat: tambah CartBinding dan GetPage untuk CartPage di AppPages

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
+import '../controller/cart_controller.dart';
 import '../controller/product_controller.dart';
+import '../view/pages/cart_page.dart';
 import '../view/pages/product_detail_page.dart';
 import '../view/pages/product_page.dart';
 import 'app_routes.dart';
@@ -9,6 +11,18 @@ class ProductBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<ProductController>(() => ProductController());
+    // CartController juga dipasang di sini agar tersedia di seluruh app
+    Get.put<CartController>(CartController(), permanent: true);
+  }
+}
+
+// Binding untuk CartPage
+class CartBinding extends Bindings {
+  @override
+  void dependencies() {
+    // CartController sudah dipasang permanent di ProductBinding,
+    // Get.find() cukup untuk mengaksesnya kembali
+    Get.find<CartController>();
   }
 }
 
@@ -20,6 +34,14 @@ class AppPages {
       page: () => ProductPage(),
       binding: ProductBinding(),
     ),
-    GetPage(name: AppRoutes.detail, page: () => ProductDetailPage()),
+    GetPage(
+      name: AppRoutes.detail,
+      page: () => ProductDetailPage(),
+    ),
+    GetPage(
+      name: AppRoutes.cart,
+      page: () => CartPage(),
+      binding: CartBinding(),
+    ),
   ];
 }
